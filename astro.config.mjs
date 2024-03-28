3af2f7c646ea6a63bd36aa386b1c7bee649764e6
mengpeng <pengmeng@yangqianguan.com>
2024-03-28 22:36:54 +0800

    feat(base): 添加 baselayout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [tailwind()],
+    vite: {
+        resolve: {
+          alias: {
+            '~': path.resolve(__dirname, './src'),
+          },
+        },
+      },
+});
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
