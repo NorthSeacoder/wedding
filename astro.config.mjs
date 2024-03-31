6d22608c4e4d7551cb17468679a5dd194dc6e545
mengpeng <pengmeng@yangqianguan.com>
2024-03-31 16:00:11 +0800

    fix(ts): 增加 wx umami 相关 declare

--- astro.config.mjs
+++ astro.config.mjs
@@ -49,5 +49,6 @@ export default defineConfig({
                 '~': path.resolve(__dirname, './src')
             }
         }
-    }
+    },
+    
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -2,3 +2,6 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="vite/client" />
+
+declare const umami: any;
+declare const wx: any;
