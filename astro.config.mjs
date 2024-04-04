322a86fd74e721453d914b8128daac359d427045
mengpeng <pengmeng@yangqianguan.com>
2024-04-04 11:28:58 +0800

    feat(photo): 第一版 photo 组件

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,26 @@
-import {defineConfig,squooshImageService} from 'astro/config';
+import {defineConfig, squooshImageService} from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
-
 import path from 'path';
 import {fileURLToPath} from 'url';
-
 import {SITE} from './src/utils/config.ts';
 import tasks from './src/utils/tasks';
-
+import react from '@astrojs/react';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 // https://astro.build/config
 export default defineConfig({
     site: SITE.site,
     base: SITE.base,
     trailingSlash: SITE.trailingSlash ? 'always' : 'never',
-
+    server:{
+      host:'local.mengpeng.tech'
+    },
     integrations: [
         tailwind({
-            applyBaseStyles: false
+            applyBaseStyles: false,
+            nesting: true
         }),
         sitemap(),
         icon({
@@ -37,18 +39,17 @@ export default defineConfig({
                 ]
             }
         }),
-        tasks()
+        tasks(),
+        react()
     ],
     image: {
-        service: squooshImageService(),
-      },
-      
+        service: squooshImageService()
+    },
     vite: {
         resolve: {
             alias: {
                 '~': path.resolve(__dirname, './src')
             }
         }
-    },
-    
+    }
 });
