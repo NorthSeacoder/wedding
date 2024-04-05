0b63bcd6d8efdbff4d5d40b260a3df29ff6dcafb
mengpeng <pengmeng@yangqianguan.com>
2024-04-05 13:40:02 +0800

    feat: 增加首页图

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,7 @@ module.exports = {
         sans: ['var(--aw-font-sans)', ...defaultTheme.fontFamily.sans],
         serif: ['var(--aw-font-serif)', ...defaultTheme.fontFamily.serif],
         heading: ['var(--aw-font-heading)', ...defaultTheme.fontFamily.sans],
+        title:['title-font', ...defaultTheme.fontFamily.sans]
       },
     },
   },
